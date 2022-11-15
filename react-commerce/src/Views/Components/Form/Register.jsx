4b180928727169d14fa7174d07232b30a545130e
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-11-15 11:21:24 +0100

    formulario proceso

--- react-commerce/src/Views/Components/Form/Register.jsx
+++ react-commerce/src/Views/Components/Form/Register.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import React from "react";
+import { v4 } from "uuid";
 
-import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
-
   
+const [state,setState] = useState({
+  id:"",
+  name:"",
+  lastname:"",
+  email:"",
+  password:""
+})
+
+const changeInput = (e) =>{
+  const stateCopy = {...state}
+  stateCopy[e.target.name] = e.target.value  
+  setState(stateCopy)
+}
+console.log(state)
+
+
+
+  const submit = (e) =>{
+    e.preventDefault()
+    state.id=v4()
+    fetch('http://localhost:8000/Users' ,{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(state)
+    })
+  }
+
 
   //show content in json users
-  /*  async function obtenUser() {
+ /*   async function obtenUser() {
     let respuesta = await fetch("http://localhost:8000/Users");
     let datos = await respuesta.json();
     console.table(datos);
@@ -18,15 +45,15 @@ const Register = () => {
 
   return (
     <>
-    <br></br>
-    <br></br>
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text"{...register("name")} placeholder="enter your name"/>
-      <input type="email" {...register("email")} placeholder="enter your email"/>
-      <input type="tel" {...register("phone")} placeholder="enter your phone"/>
-      <input type="password" {...register("password")} placeholder="enter your password"/>
-      <input type="submit" />
+    <form onSubmit={submit}>
+    <input type="text" name="name" value={state.name} onChange={changeInput}/>
+    <input type="text" name="lastname" value={state.lastname} onChange={changeInput}/>
+    <input type="text" name="email" value={state.email} onChange={changeInput}/>
+    <input type="text" name="password" value={state.password} onChange={changeInput}/>
+
+<input type="submit" value='submit'/>
     </form>
+      
     </>
   );
 };
