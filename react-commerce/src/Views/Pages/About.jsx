bb16fa3a0e011f2ec8196420d76224aa4c24afa1
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-11-25 21:11:17 +0100

    vamos a la ultima lesson

--- react-commerce/src/Views/Pages/About.jsx
+++ react-commerce/src/Views/Pages/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import './../../design/About.css'
 const About = () => {
   return (
    <>
-   <h1>ABOUT</h1>
+   <div id="container">
+  <div id="success-box">
+    <div class="dot"></div>
+    <div class="dot two"></div>
+    <div class="face">
+      <div class="eye"></div>
+      <div class="eye right"></div>
+      <div class="mouth happy"></div>
+    </div>
+    <div class="shadow scale"></div>
+    <div class="message"><h1 class="alert">About us</h1>
+    <h6>Hello, my name is Mimika Ackerman, designer of the illustrations that you can see in the store session. 
+If you want a custom design please fill out the form below and we will contact you.</h6>
+    </div>
+   
+  </div>
+</div>
    </>
   )
 }
