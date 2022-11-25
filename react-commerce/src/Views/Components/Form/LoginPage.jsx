3c9d6ea384868e79d1773ce8d80aedbd47031c27
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-11-25 17:20:27 +0100

    dead

--- react-commerce/src/Views/Components/Form/LoginPage.jsx
+++ react-commerce/src/Views/Components/Form/LoginPage.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { LoginContext } from "../../../context/LoginContext";
-
 import { fetchUsers } from "../../../Api/fetchUsers";
-import { useState, useEffect } from "react";
-
-
-
+import { LoginContext } from "../../../context";
 
 const LoginPage = () => {
   const { login } = useContext(LoginContext);
 
+  const [users, setUsers] = useState([]);
+  const url = " http://localhost:8000/Users";
+  
+
+  useEffect(() => {
+    const connection = async () => {
+      const data = await fetchUsers(url);
+      setUsers(data);
+    };
+    connection();
+  }, [url]);
 
   const onLogin = () => {
     login("mimika ackerman");
--- react-commerce/src/context/LoginContext.jsx
+++ react-commerce/src/context/LoginContext.jsx
@@ -1,2 +1,2 @@
-import React from "react";
-export const LoginContext = React.createContext();
+import { createContext } from "react";
+export const LoginContext = createContext()
